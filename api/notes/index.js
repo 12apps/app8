7411dd02c1a7fa4899e5c3ecb7360b72795c81b0
teleaziz <abdz.abbas@gmail.com>
2014-12-19 14:49:56 -0800

    notes api endpoints CRUDI

--- api/notes/index.js
+++ api/notes/index.js
@@ -2,6 +2,12 @@ var express= require('express');
 var router = express.Router();
 var notesController = require('./note.controller');
 
+function sanitizeBody(req, res, next){
+  if (req.body._id) {delete req.body._id};
+  req.test = "in the middleware"
+  next()
+}
+
 // /api/v1/notes/ --> get all notes isArray = true
 router.get('/', notesController.index);
 
@@ -9,10 +15,10 @@ router.get('/', notesController.index);
 router.get('/:id' , notesController.read);
 
 // create note
-router.post('/' , notesController.create);
+router.post('/' ,sanitizeBody, notesController.create);
 
 // update note
-router.put('/:id' , notesController.update);
+router.put('/:id' ,sanitizeBody, notesController.update);
 
 // delete one note
 router.delete('/:id' , notesController.destroy);
--- api/notes/note.controller.js
+++ api/notes/note.controller.js
@@ -2,18 +2,18 @@ var mongoose = require('mongoose');
 var Note = require('./note.model');
 
 exports.index = function(req, res){
-Note.find({} , function(err,notes){
-  if(err) return handleError(err , res);
-  res.status(200).json(notes); // array of notes
-})
+  Note.find({} , function(err,notes){
+    if(err) return handleError(err , res);
+    res.status(200).json(notes); // array of notes
+  })
 }
 
 
 exports.read = function(req, res){
-Note.findOne({_id: req.params.id }, function(err , note){
-  if(err) return handelError(err, res);
+  Note.findOne({_id: req.params.id }, function(err , note){
+    if(err) return handelError(err, res);
     res.status(200).json(note);
-})
+  })
 }
 
 // assume sending in the req.body 
@@ -23,21 +23,27 @@ Note.findOne({_id: req.params.id }, function(err , note){
 //}
 
 exports.create = function(req, res){
-  if( req.body._id) {delete req.body._id};
- var note = new Note(req.body);
+  var note = new Note(req.body);
   note.save(function(err , savedNote){
     if(err) return handelError(err, res);
     res.status(201).json(note);
   })
 }
 
-
+// {body: 'this is fun'}
 exports.update = function(req, res){
-console.log(' update hit');
+  Note.findByIdAndUpdate(req.params.id , req.body,function(err, note){
+    if(err) return handelError(err, res);
+    res.status(201).json(note);
+  });
 }
 
 exports.destroy = function(req, res){
-console.log(' delete hit');
+  Note.findByIdAndRemove(req.params.id , function(err , deletedNote){
+    if(err) return handelError(err, res);
+    res.status(200).json(deletedNote);
+  
+  })
 }
 
 function handelError(err ,res){
